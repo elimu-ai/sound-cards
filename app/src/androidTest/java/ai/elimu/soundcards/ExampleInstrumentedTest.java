9756d0893ed803d9c2356f00488227d025a6b151
Nya <info@elimu.ai>
2022-02-27 15:15:47 +0800

    refactor: Rename package to ai.elimu

--- app/src/androidTest/java/ai/elimu/soundcards/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ai/elimu/soundcards/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ai.elimu.imagepicker", appContext.getPackageName());
+        assertEquals("ai.elimu.soundcards", appContext.getPackageName());
     }
 }
