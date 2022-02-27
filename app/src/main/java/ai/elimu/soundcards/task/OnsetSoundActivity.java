0fa08c055e52f4bdd98477db9a32bbcb04d34775
Nya <info@elimu.ai>
2022-02-27 16:07:37 +0800

    refactor: Remove deprecated code

--- app/src/main/java/ai/elimu/soundcards/task/OnsetSoundActivity.java
+++ app/src/main/java/ai/elimu/soundcards/task/OnsetSoundActivity.java
@@ -82,66 +82,7 @@ public class OnsetSoundActivity extends AppCompatActivity {
     private void loadNextTask() {
         Log.i(getClass().getName(), "loadNextTask");
 
-        if (wordsCorrectlySelected.size() == wordsWithImage.size()) {
-            // TODO: show congratulations page
-            finish();
-            return;
-        }
-
-        int progress = (wordsCorrectlySelected.size() * 100) / wordsWithImage.size();
-        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(progressBar, "progress", progress);
-        objectAnimator.setDuration(1000);
-        objectAnimator.start();
-
-        final WordGson alt1Word = wordsWithImage.get(wordsCorrectlySelected.size());
-        Log.i(getClass().getName(), "alt1Word.getText(): " + alt1Word.getText());
-
-//        List<ImageGson> alt1Images = ContentProvider.getAllImagesLabeledByWord(alt1Word);
-//        final ImageGson alt1Image = alt1Images.get((int) (Math.random() * alt1Images.size()));
-//        File alt1ImageFile = MultimediaHelper.getFile(alt1Image);
-//        Bitmap alt1ImageBitmap = BitmapFactory.decodeFile(alt1ImageFile.getAbsolutePath());
-//        final String alt1ImageDominantColor = alt1Image.getDominantColor();
-//        final int alt1ColorIdentifier = parseRgbColor(alt1ImageDominantColor, 20);
-
-        // TODO: fetch words with different onset sound
-        List<WordGson> otherWords = new ArrayList<>(wordsWithImage);
-        otherWords.remove(alt1Word);
-        final WordGson alt2Word = otherWords.get((int) (Math.random() * otherWords.size()));
-        Log.i(getClass().getName(), "alt2Word.getText(): " + alt2Word.getText());
-
-//        List<ImageGson> alt2Images = ContentProvider.getAllImagesLabeledByWord(alt2Word);
-//        final ImageGson alt2Image = alt2Images.get((int) (Math.random() * alt2Images.size()));
-//        File alt2ImageFile = MultimediaHelper.getFile(alt2Image);
-//        Bitmap alt2ImageBitmap = BitmapFactory.decodeFile(alt2ImageFile.getAbsolutePath());
-//        final String alt2ImageDominantColor = alt2Image.getDominantColor();
-//        final int alt2ColorIdentifier = parseRgbColor(alt2ImageDominantColor, 20);
-
-        final boolean isAlt1Correct = (Math.random() > .5);
-//        if (isAlt1Correct) {
-//            alt1CardView.setCardBackgroundColor(alt1ColorIdentifier);
-//            alt1ImageView.setImageBitmap(alt1ImageBitmap);
-//            alt2CardView.setCardBackgroundColor(alt2ColorIdentifier);
-//            alt2ImageView.setImageBitmap(alt2ImageBitmap);
-//        } else {
-//            alt1CardView.setCardBackgroundColor(alt2ColorIdentifier);
-//            alt1ImageView.setImageBitmap(alt2ImageBitmap);
-//            alt2CardView.setCardBackgroundColor(alt1ColorIdentifier);
-//            alt2ImageView.setImageBitmap(alt1ImageBitmap);
-//        }
-
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_swing_up_left);
-        alt1CardView.startAnimation(animation);
-        alt2CardView.startAnimation(animation);
-        MediaPlayerHelper.play(getApplicationContext(), R.raw.shuffle_cards);
-
-        alt1CardView.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                MediaPlayerHelper.play(getApplicationContext(), R.raw.which_word_begins_with_this_sound);
-
-                // TODO
-            }
-        }, 1000);
+        // TODO
     }
 
     private int parseRgbColor(String input, int alpha) {
