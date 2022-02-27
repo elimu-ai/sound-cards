2ca00600e8a7d367f8bd1c3e6a79635454b40475
Nya <info@elimu.ai>
2022-02-27 15:19:59 +0800

    refactor: Skip READ_EXTERNAL_STORAGE

--- app/src/main/java/ai/elimu/soundcards/MainActivity.java
+++ app/src/main/java/ai/elimu/soundcards/MainActivity.java
@@ -1,20 +1,14 @@
 package ai.elimu.soundcards;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
-import ai.elimu.contentprovider.ContentProvider;
+import androidx.appcompat.app.AppCompatActivity;
+
 import ai.elimu.soundcards.task.OnsetSoundActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,35 +22,9 @@ public class MainActivity extends AppCompatActivity {
         Log.i(getClass().getName(), "onStart");
         super.onStart();
 
-        // Ask for permissions
-        int permissionCheckWriteExternalStorage = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (permissionCheckWriteExternalStorage != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
-            return;
-        }
-
-        ContentProvider.initializeDb(this);
-
         Intent intent = new Intent(this, OnsetSoundActivity.class);
         startActivity(intent);
 
         finish();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if ((requestCode == PERMISSION_REQUEST_READ_EXTERNAL_STORAGE)) {
-            if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                // Permission granted. Restart application
-                Intent intent = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            } else {
-                // Permission denied. Close application
-                finish();
-            }
-        }
     }
 }
