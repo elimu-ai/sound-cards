24e4a676d7a24c54b7087c7a0263896d38e2b311
tuancoltech <tuancoltech@gmail.com>
2025-05-20 12:28:55 +0700

    Convert IpaToAndroidResourceConverter to Kotlin

--- app/src/main/java/ai/elimu/soundcards/util/IpaToAndroidResourceConverter.kt
+++ app/src/main/java/ai/elimu/soundcards/util/IpaToAndroidResourceConverter.kt
@@ -1,10 +1,9 @@
-package ai.elimu.soundcards.util;
+package ai.elimu.soundcards.util
 
 /**
  * Helper class for converting IPA values into a format allowed in an Android file name: [a-z], [0-9], [_]
  */
-public class IpaToAndroidResourceConverter {
-
+object IpaToAndroidResourceConverter {
     /**
      * The number 2 indicates that the X-SAMPA value is upper-case:
      *
@@ -12,69 +11,69 @@ public class IpaToAndroidResourceConverter {
      * E.g. 'e2' instead of 'E'
      * E.g. 'e2i2' instead of 'EI'
      */
-    public static String getAndroidResourceName(String ipaValue) {
-        String resourceName = ipaValue;
+    fun getAndroidResourceName(ipaValue: String?): String? {
+        var resourceName = ipaValue
 
-        if ("ɛ".equals(ipaValue)) {
+        if ("ɛ" == ipaValue) {
             // X-SAMPA: 'E'
-            resourceName = "e2";
-        } else if ("æ".equals(ipaValue)) {
+            resourceName = "e2"
+        } else if ("æ" == ipaValue) {
             // X-SAMPA: '}'
-            resourceName = "ae";
-        } else if ("ɔ".equals(ipaValue)) {
+            resourceName = "ae"
+        } else if ("ɔ" == ipaValue) {
             // X-SAMPA: 'O'
-            resourceName = "o2";
-        } else if ("t".equals(ipaValue)) {
+            resourceName = "o2"
+        } else if ("t" == ipaValue) {
             // X-SAMPA: 't'
-        } else if ("h".equals(ipaValue)) {
+        } else if ("h" == ipaValue) {
             // X-SAMPA: 'h'
-        } else if ("s".equals(ipaValue)) {
+        } else if ("s" == ipaValue) {
             // X-SAMPA: 's'
-        } else if ("i".equals(ipaValue)) {
+        } else if ("i" == ipaValue) {
             // X-SAMPA: 'i'
-        } else if ("n".equals(ipaValue)) {
+        } else if ("n" == ipaValue) {
             // X-SAMPA: 'n'
-        } else if ("r".equals(ipaValue)) {
+        } else if ("r" == ipaValue) {
             // X-SAMPA: 'r'
-        } else if ("d".equals(ipaValue)) {
+        } else if ("d" == ipaValue) {
             // X-SAMPA: 'd'
-        } else if ("l".equals(ipaValue)) {
+        } else if ("l" == ipaValue) {
             // X-SAMPA: 'l'
-        } else if ("y".equals(ipaValue)) {
+        } else if ("y" == ipaValue) {
             // X-SAMPA: 'j'
-            resourceName = "j";
-        } else if ("ʌ".equals(ipaValue)) {
+            resourceName = "j"
+        } else if ("ʌ" == ipaValue) {
             // X-SAMPA: 'V'
-            resourceName = "v2";
-        } else if ("w".equals(ipaValue)) {
+            resourceName = "v2"
+        } else if ("w" == ipaValue) {
             // X-SAMPA: 'w'
-        } else if ("m".equals(ipaValue)) {
+        } else if ("m" == ipaValue) {
             // X-SAMPA: 'm'
-        } else if ("g".equals(ipaValue)) {
+        } else if ("g" == ipaValue) {
             // X-SAMPA: 'g'
-        } else if ("c".equals(ipaValue)) {
+        } else if ("c" == ipaValue) {
             // X-SAMPA: 'k'
-        } else if ("b".equals(ipaValue)) {
+        } else if ("b" == ipaValue) {
             // X-SAMPA: 'b'
-        } else if ("p".equals(ipaValue)) {
+        } else if ("p" == ipaValue) {
             // X-SAMPA: 'p'
-        } else if ("f".equals(ipaValue)) {
+        } else if ("f" == ipaValue) {
             // X-SAMPA: 'f'
-        } else if ("k".equals(ipaValue)) {
+        } else if ("k" == ipaValue) {
             // X-SAMPA: 'k'
-        } else if ("v".equals(ipaValue)) {
+        } else if ("v" == ipaValue) {
             // X-SAMPA: 'v'
-        } else if ("z".equals(ipaValue)) {
+        } else if ("z" == ipaValue) {
             // X-SAMPA: 'z'
-        } else if ("dʒ".equals(ipaValue)) {
+        } else if ("dʒ" == ipaValue) {
             // X-SAMPA: 'dZ'
-            resourceName = "dz2";
-        } else if ("ks".equals(ipaValue)) {
+            resourceName = "dz2"
+        } else if ("ks" == ipaValue) {
             // X-SAMPA: 'ks'
-        } else if ("kw".equals(ipaValue)) {
+        } else if ("kw" == ipaValue) {
             // X-SAMPA: 'kw'
         }
 
-        return resourceName;
+        return resourceName
     }
 }
