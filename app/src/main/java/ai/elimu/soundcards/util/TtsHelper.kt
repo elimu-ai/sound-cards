c66941bc6b40854cfc9989f65654605fa4b1f6bf
tuancoltech <tuancoltech@gmail.com>
2025-05-20 12:29:35 +0700

    Convert TtsHelper to Kotlin

--- app/src/main/java/ai/elimu/soundcards/util/TtsHelper.kt
+++ app/src/main/java/ai/elimu/soundcards/util/TtsHelper.kt
@@ -1,20 +1,18 @@
-package ai.elimu.soundcards.util;
+package ai.elimu.soundcards.util
 
-import android.content.Context;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
+import ai.elimu.soundcards.SoundCardApplication
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import android.util.Log
 
-import ai.elimu.soundcards.SoundCardApplication;
+object TtsHelper {
+    fun speak(context: Context?, text: String?) {
+        Log.i(TtsHelper::class.java.getName(), "speak")
 
-public class TtsHelper {
+        Log.i(TtsHelper::class.java.getName(), "text: " + text)
 
-    public static void speak(Context context, String text) {
-        Log.i(TtsHelper.class.getName(), "speak");
-
-        Log.i(TtsHelper.class.getName(), "text: " + text);
-
-        SoundCardApplication application = (SoundCardApplication) context;
-        TextToSpeech tts = application.getTts();
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        val application = context as SoundCardApplication
+        val tts = application.tts
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
     }
 }
